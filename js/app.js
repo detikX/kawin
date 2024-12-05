a0e1c75670ee8e65741ead84faf24cad81c48635
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-12-05 15:01:20 +0700

    update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,35 @@ new WOW().init();
 $(".bubble-text p, .bubble-text small").addClass('wow animate__animated animate__fadeIn')
 
 
+
+
+
+const pauseVar = "fa-pause-circle";
+const playVar = "fa-play-circle";
+
+const btns = document.querySelectorAll(".all");
+
+function onChange(event) {
+    const buttonElement = event.currentTarget;
+
+    const isPlayButton = buttonElement.classList.contains(playVar);
+
+    if (isPlayButton) {
+        buttonElement.classList.remove(playVar);
+        buttonElement.classList.add(pauseVar);
+        $("#play-button").removeClass("animate__infinite");
+        $("#myAudio").get(0).play();
+    } else {
+        buttonElement.classList.remove(pauseVar);
+        buttonElement.classList.add(playVar);
+        $("#play-button").addClass("animate__infinite");
+        $("#myAudio").get(0).pause();
+    }
+
+    // You can also use .toggle function on classList as mentioned by the person in other answer
+}
+
+// query selector all returns a list of nodes, therefore we need to iterate over it and attach an event listener to each button seperatly
+btns.forEach((btn) => {
+    btn.addEventListener("click", onChange);
+});
